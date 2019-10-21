3b371bbb48b306f31e1d7a967d06438fbd9e665b
thunter <thunter@covermymeds.com>
2019-10-21 16:35:07 -0400

    Implemented enum for status into posts for approval workflow

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_08_175234) do
+ActiveRecord::Schema.define(version: 2019_10_21_191254) do
 
   create_table "posts", force: :cascade do |t|
     t.date "date"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_07_08_175234) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "status", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
